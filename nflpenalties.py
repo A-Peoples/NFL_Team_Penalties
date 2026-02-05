88d8dfeb879c4e2c8c4e73f2454539611277ae0a
Ah-Lon Peoples, II <143513372+A-Peoples@users.noreply.github.com>
2026-02-05 12:47:15 -0500

    Update nflpenalties.py

--- nflpenalties.py
+++ nflpenalties.py
@@ -28,7 +28,7 @@ with tab_yearspan:
   st.line_chart(data=team_pen, x='season', y='penalty', x_label='Season', y_label='Penalties', color=color_filt)
 with tab_player:
   st.header(team_filt + ' Top 25 Player Penalties')
-  pen_person = pen_person.loc[(pen_person['penalty_team'] == team_filt)]
+  pen_person = pen_person.loc[(pen_person['season'] == year_filt) & (pen_person['penalty_team'] == team_filt)]
   st.bar_chart(data=pen_person, x='penalty_player_name', y='penalty', x_label='Total Penalties', y_label='Player', color=color_filt, horizontal=True, sort=True, stack=None, width="stretch", height="content", use_container_width=None)
 with tab_types:
   st.header(team_filt + ' Common Team Penalties')
