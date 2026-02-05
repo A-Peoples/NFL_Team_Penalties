08b78b0f510b661d2d047de68eaf09a733a0fdd0
Ah-Lon Peoples, II <143513372+A-Peoples@users.noreply.github.com>
2026-02-05 11:10:21 -0500

    Update nflpenalties.py

--- nflpenalties.py
+++ nflpenalties.py
@@ -9,8 +9,8 @@ import streamlit as st
 st.set_page_config(page_title='NFL Penalty Charting', layout="wide")
 @st.cache_data()
 
-
-team_list = sorted(pbp['penalty_team'].unique().dropna().tolist())
+team_pen = pd.read_csv("https://raw.githubusercontent.com/A-Peoples/NFL_Team_Penalties/refs/heads/main/penalty_count.csv")
+team_list = sorted(team_pen['penalty_team'].unique().dropna().tolist())
 
 team_filt = st.sidebar.selectbox('Choose team: ', team_list)
 year_filt = st.slider('Year Details: ', 2023, 2024, 2024)
@@ -20,7 +20,7 @@ pbp_filt = pbp.loc[((pbp['home_team'] == team_filt) | (pbp['away_team'] == team_
 tab_yearspan, tab_types, tab_player, tab_positions = st.tabs(['Team Penalties Timespan', 'Common Team Penalties', 'Top 20 Player Penalties', 'Position Penalties'])
 with tab_yearspan:
   st.header('Team Penalties Timespan')
-  team_pen = pd.read_csv("https://raw.githubusercontent.com/A-Peoples/NFL_Team_Penalties/refs/heads/main/penalty_count.csv")
+  
   team_pen = team_pen.loc[team_pen['team'] == "team_filt"]
   st.line_chart(data=team_pen, x='season', y='penalty_count', x_label='Season', y_label='Penalties', width="stretch", height="content", use_container_width=None)
 with tab_player:
