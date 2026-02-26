90bfe018396f9ea66b6ba8e7aad5d0944e0c494e
Ah-Lon Peoples, II <143513372+A-Peoples@users.noreply.github.com>
2026-02-26 15:40:33 -0500

    Update nflpenalties.py

--- nflpenalties.py
+++ nflpenalties.py
@@ -19,8 +19,8 @@ team_pen, pen_person, pen_type, colors, names = load_data()
 team_list = names['team_name'].dropna().unique().tolist()
 team_filt = st.sidebar.selectbox('Choose team: ', team_list)
 names_var = names.loc[names['team_name'] == team_filt]
-team_fun_filt = names_var['team_abbr']
-st.header(str(names_var['team_abbr']))
+team_fun_filt = names_var['team_abbr'].iloc[0]
+st.header(str(names_var['team_fun_filt']))
 year_filt = st.slider('Year Details: ', 2016, 2024, 2024)
 colors_sing = colors.loc[colors['team_abbr'] == team_fun_filt]
 color_filt = colors_sing['team_color'].iloc[0]
