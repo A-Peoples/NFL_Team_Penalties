2adf09fdfa84d58964fef6584bf7fe417855d9b2
Ah-Lon Peoples, II <143513372+A-Peoples@users.noreply.github.com>
2026-03-14 11:07:11 -0400

    Update nflpenalties.py

--- nflpenalties.py
+++ nflpenalties.py
@@ -20,11 +20,12 @@ team_list = names['team_name'].dropna().unique().tolist()
 team_filt = st.sidebar.selectbox('Choose team: ', team_list)
 names_var = names.loc[names['team_name'] == team_filt]
 team_fun_filt = names_var['team_abbr'].iloc[0]
+st.subheader("Article: https://medium.com/@ahlon.aj.peoples/exploratory-data-analysis-in-streamlit-how-penalties-impact-teams-and-games-in-the-nfl-f0c8791db96a")
 year_filt = st.slider('Year Details: ', 2016, 2025, 2025)
 colors_sing = colors.loc[colors['team_abbr'] == team_fun_filt]
 color_filt = colors_sing['team_color'].iloc[0]
 tab_yearspan, pen_yards, tab_types, tab_player = st.tabs(['Team Penalties Timespan', 'Penalty Yards Timespan', 'Common Team Penalties', 'Player Penalty Count'])
-st.subheader("Article: https://medium.com/@ahlon.aj.peoples/exploratory-data-analysis-in-streamlit-how-penalties-impact-teams-and-games-in-the-nfl-f0c8791db96a")
+
 with tab_yearspan:
   st.header(team_filt + ' Team Penalties Timespan')
   
